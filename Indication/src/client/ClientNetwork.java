e3325c655a6788b59f6727d88fbd9e5178dbe866
Christopher Patton <chpatton013@gmail.com>
2014-05-11 13:34:31 -0700

    add javadocs to ClientNetwork

--- Indication/src/client/ClientNetwork.java
+++ Indication/src/client/ClientNetwork.java
@@ -2,34 +2,60 @@ package client;
 
 import com.lloseng.ocsf.client.AbstractClient;
 
+/**
+ * The networking layer for the client application.
+ * Handles all incoming and outgoing network traffic by communicating directly
+ * with the ClientController.
+ */
 class ClientNetwork extends AbstractClient
 {
     private ClientController ctrl;
-    private ConnectionToClient server;
 
+    /**
+     * Construct a new ClientNetwork on the give port.
+     *
+     * @param  port  the port number to connect to the server on
+     */
     public ClientNetwork(int port)
     {
         super(port);
     }
 
+    /**
+     * Return this instance's reference to the ClientController.
+     *
+     * @return the internal reference to the ClientController.
+     * @see    ClientController
+     */
     public ClientController getClientController()
     {
     }
 
-    public ConnectionToClient getServer()
-    {
-    }
-
+    /**
+     * Send an object to the server.
+     *
+     * @param  obj   the object to send as a message.
+     */
     public void send(Object obj)
     {
     }
 
+    /**
+     * Handle reception of an object from the server.
+     *
+     * @param  obj   incoming object from server.
+     */
     public void receive(Object obj)
     {
     }
 
+    /**
+     * Handles a message sent from the server to this client.
+     *
+     * @param msg   the message sent.
+     */
     @Override
-    public void handleMessageFromServer(Object msg)
+    protected void handleMessageFromServer(Object msg)
     {
     }
 }
