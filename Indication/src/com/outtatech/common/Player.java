a8769a73b9b02b9fe7b859156dd1179c84c1e353
bschalich <bennettschalich@pcp130449pcs.wireless.calpoly.edu>
2014-05-13 10:28:01 -0700

    Updated class diagrams

---
+++ Indication/src/com/outtatech/common/Player.java
@@ -0,0 +1,20 @@
+package com.outtatech.common;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * The Player interface represents a player in the game
+ * @author bennettschalich
+ */
+public interface Player 
+{
+   /**
+    * Returns the id of the player
+    * @return The unique id of the player
+    */
+   int getPlayerId(); 
+}
