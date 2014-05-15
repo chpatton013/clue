952f09018199d8543323c11c3626c72a19d55805
bschalich <bennettschalich@bennett-schalichs-macbook-pro-2.local>
2014-05-14 21:58:49 -0700

    Updated ActionCard class skeletons

--- Indication/src/com/outtatech/common/SuperSleuth.java
+++ Indication/src/com/outtatech/common/SuperSleuth.java
@@ -6,10 +6,16 @@
 
 package com.outtatech.common;
 
-
 /**
  * The SuperSleuth class represents a type of ActionCard that represents an 
- * SuperSleuth action.
+ * SuperSleuth action. A SuperSleuth action does on of the following:
+ * A player of your choice:
+ *   "Shows you one female Suspect Card." (1)
+ *   "Show you one male Suspect Card." (1)
+ *   "Shows you one flying Vehicle Card." (1)
+ *   "Shows you one blue Vehicle Card." (1)
+ *   "Shows you one southern Destination Card." (1)
+ *   "Shows you one western Destination Card." (1)
  * @author bennettschalich
  */
 public class SuperSleuth extends ActionCard
