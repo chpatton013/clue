d9ccc3e88b0ac569bde3e374ff82de7679c631a6
AzureEnigma <dmangin@calpoly.edu>
2014-05-25 19:16:24 -0700

    Overhead application files added

---
+++ src/ServerApplication.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.outtatech.server.*;
+
+/**
+ *
+ * @author dmangin
+ */
+public class ServerApplication {
+    
+    public ServerApplication() {
+        ServerNetwork network = new ServerNetwork(55555);
+        ServerController server = new ServerController(network);
+    }
+    
+    public static void main(String[] args) {
+        ServerApplication app = new ServerApplication();
+    }
+}
