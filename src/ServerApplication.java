37c81c7d6899a850d3035dc6b1627342744f57f2
Christopher Patton <chpatton013@gmail.com>
2014-05-25 20:12:50 -0700

    set server controller

--- src/ServerApplication.java
+++ src/ServerApplication.java
@@ -15,6 +15,7 @@ public class ServerApplication {
     public ServerApplication() {
         ServerNetwork network = new ServerNetwork(55555);
         ServerController server = new ServerController(network);
+        server.setServerController(network);
     }
     
     public static void main(String[] args) {
