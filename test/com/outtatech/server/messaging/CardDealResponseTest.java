2a2ad56f5e41cd433e91ac846f7d59bbc2292461
mgfeldman <MarkFeldman@pcp061782pcs.wireless.calpoly.edu>
2014-05-26 17:28:27 -0700

    Fix some more Tests, remove final ints for Difficulty

--- test/com/outtatech/server/messaging/CardDealResponseTest.java
+++ test/com/outtatech/server/messaging/CardDealResponseTest.java
@@ -35,9 +35,9 @@ public class CardDealResponseTest
         System.out.println("Testing getCards of CardDealResponse ");
         
         List<Card> list = new ArrayList<Card>();
-        list.add(new DestinationCard(DestinationID.NIAGRA_FALLS, CardColor.RED));
+        list.add(new DestinationCard(DestinationID.NIAGRA_FALLS));
         list.add(new VehicleCard(VehicleID.AIRLINER, CardColor.RED));
-        list.add(new SuspectCard(SuspectID.MUSTARD, CardColor.RED));
+        list.add(new SuspectCard(SuspectID.MUSTARD));
         
         CardDealResponse response = new CardDealResponse(list);
         List<Card> expResult = list;
