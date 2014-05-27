3d6e2b74854e12750a95c3db4ac43a0d2c34da9c
Steven Chiu <steven.y.chiu@gmail.com>
2014-05-26 18:22:55 -0700

    beginning comment removed from test folder

--- test/com/outtatech/server/messaging/KickPlayerResponseTest.java
+++ test/com/outtatech/server/messaging/KickPlayerResponseTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.outtatech.server.messaging;
 import com.outtatech.server.messaging.KickPlayerResponse;
 
